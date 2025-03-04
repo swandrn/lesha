a1e167fcc254aade9aff179db274e628f84e207c
swandrn <159421702+swandrn@users.noreply.github.com>
2025-03-04 14:43:38 +0100

    feat(database): initialize database
    feat(migration): auto migrate to latest database

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"lesha.com/server/internal/entity"
+)
 
 func main() {
-	fmt.Println("hello world")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file ", err.Error())
+	}
+	dsn := os.Getenv("DB_URL")
+	fmt.Println("dsn: ", dsn)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Migrating...")
+	err = db.AutoMigrate(&entity.Channel{}, &entity.Friendship{}, &entity.Media{}, &entity.Message{}, &entity.Reaction{}, &entity.Server{}, &entity.User{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Migration successful!")
 }
